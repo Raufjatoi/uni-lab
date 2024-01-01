ea57df6d11eefdf90ca1518b9a75dfee540c91a3
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-01-01 15:21:51 +0500

    1/1/24        3:21 PM

---
+++ q#3.py
@@ -0,0 +1,10 @@
+### Qno 3
+a=10
+b=12
+a+=b**2
+print(not(a<100)and(b>a))
+
+### Qno 4 
+a=50//3%2**2
+b=a**0
+print(not(a<b and a==0))
---
+++ q#4.py
@@ -0,0 +1,23 @@
+## Qno1 
+x = 0
+a = 5
+b = 5
+if a > 0:
+     if b < 0:
+         x = x + 5
+     elif a > 5:
+      x = x + 4
+     else:
+      x = x + 3
+else:
+     x = x + 2
+print(x)
+
+#Qno4
+a = 25
+if a > 15:
+    print("Hi") 
+    if a <= 30:
+         print("Hello") 
+    else:
+      print("Good bye!")
